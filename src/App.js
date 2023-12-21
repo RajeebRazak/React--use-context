f86ac1b16ea69234302b54ca49ef39f522617f2a
RajeebRazak <rajeebrazak786@gmail.com>
2023-12-21 16:46:35 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles.css";
+import Products from "./Components/Product";
+import Cart from "./Components/Cart";
+import { data } from "./Data";
+
+export const BooksContext = createContext();
 
 function App() {
+  const [state, setState] = useState({
+    booklist: data.products,
+    cart: []
+  });
+
+  const addToCart = (book) => {
+    setState({
+      ...state,
+      cart: state.cart.find((cartItem) => cartItem.id === book.id)
+        ? state.cart.map((cartItem) =>
+            cartItem.id === book.id
+              ? { ...cartItem, count: cartItem.count + 1 }
+              : cartItem
+          )
+        : [...state.cart, { ...book, count: 1 }]
+    });
+  };
+
+  const removeFromCart = (id) => {
+    setState({
+      ...state,
+      cart: state.cart.filter((cartItem) => cartItem.id !== id)
+    });
+  };
+
+  const increase = (id) => {
+    setState({
+      ...state,
+      cart: state.cart.map((cartItem) =>
+        cartItem.id === id
+          ? { ...cartItem, count: cartItem.count + 1 }
+          : cartItem
+      )
+    });
+  };
+
+  const decrease = (id) => {
+    setState({
+      ...state,
+      cart: state.cart.map((cartItem) =>
+        cartItem.id === id
+          ? { ...cartItem, count: cartItem.count > 1 ? cartItem.count - 1 : 1 }
+          : cartItem
+      )
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BooksContext.Provider
+      value={{ state, addToCart, increase, decrease, removeFromCart }}
+    >
+      <Router>
+        <div className="App">
+          <h2>
+            <span>Shopping Cart</span>
+          </h2>
+          <Routes>
+            <Route
+              path="/"
+              element={<Products addToCart={addToCart} />} // Pass addToCart function to Products component
+            />
+            <Route
+              path="/cart"
+              element={<Cart removeFromCart={removeFromCart} increase={increase} decrease={decrease} />} // Pass necessary functions to Cart component
+            />
+          </Routes>
+        </div>
+      </Router>
+    </BooksContext.Provider>
   );
 }
 
